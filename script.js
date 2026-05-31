ddd2af45752a9def27a3407ff839f4d4124ea7eb
fadlisky2012007-dev <fadlisky2012007@gmail.com>
2026-05-31 21:01:16 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// toggle clas aktive
+// toggle clas active
 
 const navbarNav = document.querySelector('.navbar-nav');
 
@@ -6,3 +6,26 @@ const navbarNav = document.querySelector('.navbar-nav');
 document.querySelector('#hamburger-menu').onclick = () => {
   navbarNav.classList.toggle('active');
 };
+
+// Form WhatsApp
+document.getElementById("wa-form").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  const nama = document.getElementById("nama").value;
+  const ukuran = document.getElementById("ukuran").value;
+  const detail = document.getElementById("detail").value;
+
+  const nomorWA = "62895700104440";
+
+  const pesan =
+`Halo, saya ingin memesan desain.
+
+Nama: ${nama}
+Ukuran: ${ukuran}
+Detail: ${detail}`;
+
+  const url =
+`https://wa.me/${nomorWA}?text=${encodeURIComponent(pesan)}`;
+
+  window.open(url, "_blank");
+});
